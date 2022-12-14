28076b7570250a4f19547971d5cf0e1586237992
Dr. Deckto <fynjy138@gmail.com>
2022-12-14 21:43:48 +0300

    sol

--- Homework/Form1.Designer.cs
+++ Homework/Form1.Designer.cs
@@ -28,73 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.ButtonResize = new System.Windows.Forms.Button();
+            this.textBoxXReadValue = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxYReadValue = new System.Windows.Forms.TextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxZReadValue = new System.Windows.Forms.TextBox();
+            this.theIntendedAnswerOutput = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.theRealAnswerOutput = new System.Windows.Forms.TextBox();
+            this.theRealAnswer = new System.Windows.Forms.TextBox();
+            this.theIntendedAnswer = new System.Windows.Forms.TextBox();
+            this.textDegreeX = new System.Windows.Forms.TextBox();
+            this.textDegreeZ = new System.Windows.Forms.TextBox();
+            this.textDegreeY = new System.Windows.Forms.TextBox();
+            this.degreeXRead = new System.Windows.Forms.TextBox();
+            this.degreeZRead = new System.Windows.Forms.TextBox();
+            this.degreeYRead = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // textBoxXReadValue
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 74);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            this.textBoxXReadValue.Location = new System.Drawing.Point(77, 12);
+            this.textBoxXReadValue.Name = "textBoxXReadValue";
+            this.textBoxXReadValue.Size = new System.Drawing.Size(149, 20);
+            this.textBoxXReadValue.TabIndex = 0;
+            this.textBoxXReadValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxReadValueKeyPress);
             // 
-            // button2
+            // textBoxX
             // 
-            this.button2.Location = new System.Drawing.Point(219, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(201, 74);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.ButtonClick);
+            this.textBoxX.Location = new System.Drawing.Point(12, 12);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.ReadOnly = true;
+            this.textBoxX.Size = new System.Drawing.Size(59, 20);
+            this.textBoxX.TabIndex = 1;
+            this.textBoxX.Text = "Ваш X ->";
             // 
-            // button3
+            // textBoxY
             // 
-            this.button3.Location = new System.Drawing.Point(426, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(201, 74);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.ButtonClick);
+            this.textBoxY.Location = new System.Drawing.Point(12, 38);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.ReadOnly = true;
+            this.textBoxY.Size = new System.Drawing.Size(59, 20);
+            this.textBoxY.TabIndex = 3;
+            this.textBoxY.Text = "Ваш Y ->";
             // 
-            // ButtonResize
+            // textBoxYReadValue
             // 
-            this.ButtonResize.Location = new System.Drawing.Point(12, 220);
-            this.ButtonResize.Name = "ButtonResize";
-            this.ButtonResize.Size = new System.Drawing.Size(201, 74);
-            this.ButtonResize.TabIndex = 3;
-            this.ButtonResize.Text = "Resize";
-            this.ButtonResize.UseVisualStyleBackColor = true;
-            this.ButtonResize.Click += new System.EventHandler(this.ButtonResizeClick);
+            this.textBoxYReadValue.Location = new System.Drawing.Point(77, 38);
+            this.textBoxYReadValue.Name = "textBoxYReadValue";
+            this.textBoxYReadValue.Size = new System.Drawing.Size(149, 20);
+            this.textBoxYReadValue.TabIndex = 2;
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(12, 64);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.ReadOnly = true;
+            this.textBoxZ.Size = new System.Drawing.Size(59, 20);
+            this.textBoxZ.TabIndex = 5;
+            this.textBoxZ.Text = "Ваш Z ->";
+            // 
+            // textBoxZReadValue
+            // 
+            this.textBoxZReadValue.Location = new System.Drawing.Point(77, 64);
+            this.textBoxZReadValue.Name = "textBoxZReadValue";
+            this.textBoxZReadValue.Size = new System.Drawing.Size(149, 20);
+            this.textBoxZReadValue.TabIndex = 4;
+            // 
+            // theIntendedAnswerOutput
+            // 
+            this.theIntendedAnswerOutput.Location = new System.Drawing.Point(128, 157);
+            this.theIntendedAnswerOutput.Name = "theIntendedAnswerOutput";
+            this.theIntendedAnswerOutput.ReadOnly = true;
+            this.theIntendedAnswerOutput.Size = new System.Drawing.Size(149, 20);
+            this.theIntendedAnswerOutput.TabIndex = 6;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(54, 90);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(100, 23);
+            this.calculate.TabIndex = 7;
+            this.calculate.Text = "Вычислить";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.Calculate);
+            // 
+            // theRealAnswerOutput
+            // 
+            this.theRealAnswerOutput.Location = new System.Drawing.Point(128, 183);
+            this.theRealAnswerOutput.Name = "theRealAnswerOutput";
+            this.theRealAnswerOutput.ReadOnly = true;
+            this.theRealAnswerOutput.Size = new System.Drawing.Size(149, 20);
+            this.theRealAnswerOutput.TabIndex = 8;
+            // 
+            // theRealAnswer
+            // 
+            this.theRealAnswer.Location = new System.Drawing.Point(12, 183);
+            this.theRealAnswer.Name = "theRealAnswer";
+            this.theRealAnswer.ReadOnly = true;
+            this.theRealAnswer.Size = new System.Drawing.Size(110, 20);
+            this.theRealAnswer.TabIndex = 9;
+            this.theRealAnswer.Text = "Вычисленный ответ";
+            // 
+            // theIntendedAnswer
+            // 
+            this.theIntendedAnswer.Location = new System.Drawing.Point(12, 157);
+            this.theIntendedAnswer.Name = "theIntendedAnswer";
+            this.theIntendedAnswer.ReadOnly = true;
+            this.theIntendedAnswer.Size = new System.Drawing.Size(110, 20);
+            this.theIntendedAnswer.TabIndex = 10;
+            this.theIntendedAnswer.Text = "Ожидаемый ответ";
+            // 
+            // textDegreeX
+            // 
+            this.textDegreeX.Location = new System.Drawing.Point(232, 12);
+            this.textDegreeX.Name = "textDegreeX";
+            this.textDegreeX.ReadOnly = true;
+            this.textDegreeX.Size = new System.Drawing.Size(54, 20);
+            this.textDegreeX.TabIndex = 11;
+            this.textDegreeX.Text = "Степень";
+            // 
+            // textDegreeZ
+            // 
+            this.textDegreeZ.Location = new System.Drawing.Point(232, 64);
+            this.textDegreeZ.Name = "textDegreeZ";
+            this.textDegreeZ.ReadOnly = true;
+            this.textDegreeZ.Size = new System.Drawing.Size(54, 20);
+            this.textDegreeZ.TabIndex = 12;
+            this.textDegreeZ.Text = "Степень";
+            // 
+            // textDegreeY
+            // 
+            this.textDegreeY.Location = new System.Drawing.Point(232, 38);
+            this.textDegreeY.Name = "textDegreeY";
+            this.textDegreeY.ReadOnly = true;
+            this.textDegreeY.Size = new System.Drawing.Size(54, 20);
+            this.textDegreeY.TabIndex = 13;
+            this.textDegreeY.Text = "Степень";
+            // 
+            // degreeXRead
+            // 
+            this.degreeXRead.Location = new System.Drawing.Point(293, 11);
+            this.degreeXRead.Name = "degreeXRead";
+            this.degreeXRead.Size = new System.Drawing.Size(48, 20);
+            this.degreeXRead.TabIndex = 14;
+            this.degreeXRead.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxReadValueKeyPress);
+            // 
+            // degreeZRead
+            // 
+            this.degreeZRead.Location = new System.Drawing.Point(292, 64);
+            this.degreeZRead.Name = "degreeZRead";
+            this.degreeZRead.Size = new System.Drawing.Size(48, 20);
+            this.degreeZRead.TabIndex = 15;
+            this.degreeZRead.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxReadValueKeyPress);
+            // 
+            // degreeYRead
+            // 
+            this.degreeYRead.Location = new System.Drawing.Point(292, 37);
+            this.degreeYRead.Name = "degreeYRead";
+            this.degreeYRead.Size = new System.Drawing.Size(48, 20);
+            this.degreeYRead.TabIndex = 16;
+            this.degreeYRead.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxReadValueKeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.ButtonResize);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(367, 376);
+            this.Controls.Add(this.degreeYRead);
+            this.Controls.Add(this.degreeZRead);
+            this.Controls.Add(this.degreeXRead);
+            this.Controls.Add(this.textDegreeY);
+            this.Controls.Add(this.textDegreeZ);
+            this.Controls.Add(this.textDegreeX);
+            this.Controls.Add(this.theIntendedAnswer);
+            this.Controls.Add(this.theRealAnswer);
+            this.Controls.Add(this.theRealAnswerOutput);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.theIntendedAnswerOutput);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.textBoxZReadValue);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxYReadValue);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxXReadValue);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button ButtonResize;
+        private System.Windows.Forms.TextBox textBoxXReadValue;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxYReadValue;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.TextBox textBoxZReadValue;
+        private System.Windows.Forms.TextBox theIntendedAnswerOutput;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox theRealAnswerOutput;
+        private System.Windows.Forms.TextBox theRealAnswer;
+        private System.Windows.Forms.TextBox theIntendedAnswer;
+        private System.Windows.Forms.TextBox textDegreeX;
+        private System.Windows.Forms.TextBox textDegreeZ;
+        private System.Windows.Forms.TextBox textDegreeY;
+        private System.Windows.Forms.TextBox degreeXRead;
+        private System.Windows.Forms.TextBox degreeZRead;
+        private System.Windows.Forms.TextBox degreeYRead;
     }
 }
 
--- Homework/Form1.cs
+++ Homework/Form1.cs
@@ -18,18 +18,49 @@ namespace Homework
             InitializeComponent();
         }
 
-        private void ButtonClick(object sender, EventArgs e)
+        private void Calculate(object sender, EventArgs e)
         {
-            var button = (Button)sender; 
+            if (!(
+                Double.TryParse(textBoxXReadValue.Text, out double x) &&
+                Double.TryParse(textBoxYReadValue.Text, out double y) && 
+                Double.TryParse(textBoxZReadValue.Text, out double z) &&
+                Double.TryParse(degreeXRead.Text, out double degreeX) &&
+                Double.TryParse(degreeYRead.Text, out double degreeY) &&
+                Double.TryParse(degreeZRead.Text, out double degreeZ)))
+            {
+                MessageBox.Show("Введено неправильное значение либо пустая строка, пожалуйста, проверьте вхродные данные");
+            }
+            else
+            {
+                x *= Math.Pow(10, degreeX);
+                y *= Math.Pow(10, degreeY); 
+                z *= Math.Pow(10, degreeZ);
 
-            button.Enabled = false;
+                double result;
+                double f = 0.33056;
+
+                double firstFraction, secondFraction, thirdFraction;
+
+                firstFraction = (Math.Pow(y, (x + 1.0))) / (Math.Pow(Math.Abs(y - 2.0), 1.0 / 3) + 3.0);
+                secondFraction = (x + (y / 2.0)) / (2.0 * Math.Abs(x + y));
+                thirdFraction = Math.Pow(x + 1, -1.0 / Math.Sin(z));
+
+                result = firstFraction + secondFraction * thirdFraction;
+
+                theIntendedAnswerOutput.Text = result.ToString();
+                theRealAnswerOutput.Text = f.ToString();
+
+            }
+
         }
 
-        private void ButtonResizeClick(object sender, EventArgs e)
+        private void TextBoxReadValueKeyPress(object sender, KeyPressEventArgs e)
         {
-            button1.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = true;
+            char number = e.KeyChar;
+            if ((number <= 47 || number >= 58) && number != 8 && number != 44 && number != 45) //цифры, клавиша BackSpace и запятая а ASCII
+            {
+                e.Handled = true;
+            }
         }
     }
 }
