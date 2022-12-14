6642fef890538267910e02e4ac18f9dcc060cab4
Dr. Deckto <fynjy138@gmail.com>
2022-12-14 20:38:20 +0300

    Добавьте файлы проекта.

---
+++ Homework/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender; 
+
+            button.Enabled = false;
+        }
+
+        private void ButtonResizeClick(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+    }
+}
